692f30b272fc33ead5fffbb89cce5bc4d83368c2
Anthy Chen <koyun.chen@gmail.com>
2023-03-16 16:57:24 -0700

    Initial commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Anthy Chen on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
